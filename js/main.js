370e28767fef66ff857467a0d4aa7909bb1ef637
Thomás Ferreira <jessicarodriguesdelgado@gmail.com>
2021-02-04 00:51:14 -0300

    estudo js

---
+++ js/main.js
@@ -0,0 +1,103 @@
+/*
+function clik() {
+    document.getElementById("Agradeço").innerHTML = "<b>Obrigada por clicar!</b>";
+    //console.log(document.getElementById("Agradeço"));
+    //alert("Obrigada por clicar!");
+}
+
+
+function redirecionar() {
+    window.open("https://globallabs.academy/");
+    //window.location.href = "https://globallabs.academy/"
+}
+
+function trocar() {
+    document.getElementById("mouse").innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar texto");
+}
+*/
+/*
+function soma(n1, n2) {
+    return n1 + n2;
+}
+var validar;
+
+function validaIdade(idade) {
+
+    if (idade >= 18) {
+        validar = true
+    } else {
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual a sua idade?");
+console.log(validaIdade(idade));
+*/
+/*
+function setReplace(Frase, nome, novo_nome) {
+    return Frase.replace(nome, novo_nome)
+}
+alert(soma(5, 10));
+alert(setReplace("vai Brasil", "Brasil", "Japão"));
+*/
+
+
+
+/*
+var d = new Date();
+alert(d.getDay() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear());
+alert(d.getHours() + ":" + d.getMinutes());
+*/
+
+/*
+var count;
+for (count = 0; count < 5; count++) {
+    alert(count);
+};
+*/
+
+
+/*
+var count = 0;
+while (count < 5) {
+    console.log(count);
+    alert(count);
+    count++;
+}
+*/
+
+/*
+var idade = prompt("Qual a sua idade?");
+var idade = 18;
+if (idade > 18) {
+    alert("Maior de idade, esse conteúdo é apropriado para você!!");
+} else {
+    alert("Menor de idade, esse conteúdo é inapropriado para sua idade!");
+}
+*/
+
+
+/*
+var fruta = [{ nome: "maçã", cor: "vermelha" }, { nome: "uva", cor: "roxa" }]
+console.log(fruta);
+alert(fruta[1].nome);
+*/
+//var lista = ("maçã", "pêra", "laranja");
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join("|"));
+
+//var nome = "Jéssica Ferreira";
+//var n1 = 29;
+//var n2 = 10;
+//var frase = "Japão é onde tem comida boa";
+//alert("Bem-vindo " + nome);
+//console.log(nome);
+//console.log(n1 + n2);
+//console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("japão", "Brasil"));
